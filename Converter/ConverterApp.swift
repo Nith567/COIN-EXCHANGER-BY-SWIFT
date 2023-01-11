ef5651b98a69e8e39b7cfd06e9ed43df6c597667
Nithin <nithinkatkam504106@gmail.com>
2023-01-11 22:30:49 +0530

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by NITHIN REDDY on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
